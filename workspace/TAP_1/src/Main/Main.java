ad90ed6e7ad95e52a7ba928bd064b3ef7a71b1b8
Joel <joel.lacambra@estudiants.urv.cat>
2022-11-25 11:41:40 +0100

    avances en la estructura base

--- workspace/TAP_1/src/Main/Main.java
+++ workspace/TAP_1/src/Main/Main.java
@@ -5,6 +5,9 @@ public class Main {
 	public static void main(String[] args) {
 	
 		System.out.println("Hello World Marc");
+		actorContext actor1 = actorContext.getInstance();
+		actorProxy hello = actor1.spawnActor(null, new HelloWorldActor());
+		hello.send(new Message(null, "Hello World. :)"));
 		
 	}
 		
